24e7009f0494dc80c13189611fb0f1887515f276
Asoji <asoji-git@hiitsdevin.mozmail.com>
2022-11-28 05:39:18 -0800

    🏗️ Gradle shenanigans [kts moment rly]

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    id("fabric-loom")
+    `maven-publish`
+    java
+}
+
+group = property("maven_group")!!
+version = property("mod_version")!!
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.wispforest.io") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+    exclusiveContent {
+        forRepository {
+            maven {
+                name = "Modrinth"
+                url = uri("https://api.modrinth.com/maven")
+            }
+        }
+        filter {
+            includeGroup("maven.modrinth")
+        }
+    }
+}
+
+
+
+dependencies {
+    minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+    mappings(loom.officialMojangMappings())
+
+    modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
+
+    modImplementation("io.wispforest:owo-lib:${property("owo_version")}")
+    include("io.wispforest:owo-sentinel:${property("owo_version")}")
+
+    // for devenv
+    modImplementation("maven.modrinth:sodium:mc1.19.2-0.4.4")
+    implementation("org.joml:joml:1.10.5")
+}
+
+tasks {
+    processResources {
+        inputs.property("version", project.version)
+        filesMatching("fabric.mod.json") {
+            expand(mutableMapOf("version" to project.version))
+        }
+    }
+
+    jar {
+        from("LICENSE")
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                artifact(remapJar) {
+                    builtBy(remapJar)
+                }
+            }
+        }
+
+        // select the repositories you want to publish to
+        repositories {
+            // uncomment to publish to the local maven
+            // mavenLocal()
+        }
+    }
+}
+
+java {
+    // Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+    // if it is present.
+    // If you remove this line, sources will not be generated.
+    withSourcesJar()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/") {
+            name = "Fabric"
+        }
+        gradlePluginPortal()
+    }
+
+    plugins {
+        id("fabric-loom") version "1.0-SNAPSHOT"
+    }
+}
