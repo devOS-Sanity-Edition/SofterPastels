1da8d0eb189297c67f9c31c404d584dbadf72e16
Naz <bluspringindohq@gmail.com>
2024-02-08 20:55:22 +0800

    Switch to mob effect instance tooltips

--- src/main/java/one/devos/nautical/SofterPastels/utils/CandyTooltipItem.java
+++ src/main/java/one/devos/nautical/SofterPastels/utils/CandyTooltipItem.java
@@ -1,6 +1,8 @@
 package one.devos.nautical.SofterPastels.utils;
 
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -8,6 +10,7 @@ import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class CandyTooltipItem extends Item {
@@ -20,7 +23,16 @@ public class CandyTooltipItem extends Item {
     @Override
     public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> list, TooltipFlag tooltipFlag) {
         list.add(Component.translatable("item.softerpastels.candies.hunger.description"));
-        PotionUtils.addPotionTooltip(itemStack, list, 1.0F, level == null ? 20.0F : level.tickRateManager().tickrate());
+
+        var effects = new LinkedList<MobEffectInstance>();
+
+        if (itemStack.getFoodComponent() != null) {
+            for (Pair<MobEffectInstance, Float> effect : itemStack.getFoodComponent().getEffects()) {
+                effects.add(effect.getFirst());
+            }
+        }
+
+        PotionUtils.addPotionTooltip(effects, list, 1.0F, level == null ? 20.0F : level.tickRateManager().tickrate());
         list.add(Component.translatable("item.softerpastels.candies.hunger.alwayseat"));
     }
 }
