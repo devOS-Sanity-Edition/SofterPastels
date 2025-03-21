be585a8645e33a9b883c93fc146cadbeae0c0653
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-03-21 13:07:03 -0700

    ⬆️ update inner pastels dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     modImplementation(libs.bundles.dependencies)
     modLocalRuntime(libs.bundles.dev.mods)
 
-    include(modImplementation("gay.asoji:innerpastels:1.3.16+rev.14e7ddb-branch.kt.1.21.main")!!)
+    include(modImplementation("gay.asoji:innerpastels:1.3.17+rev.45403a4-branch.kt.1.21.main")!!)
 }
 
 // Write the version to the fabric.mod.json
